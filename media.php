e7f29c3b016f1142a53a9a40f7023b8b9b28bf8a
llydz <legaspijohnlloyd12@gmail.com>
2024-02-27 09:50:43 +0800

    A

---
+++ media.php
@@ -0,0 +1,27 @@
+
+<?php include 'navbar.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MEDIA</title>
+</head>
+<body>
+
+    <div class="container7">
+    
+        <button class="button7">+<br>Add New Media</button>
+
+        <div class="flex-cont2">
+  <div><img src="def-img.png" alt="img"></div>
+  <div><img src="def-img.png" alt="img"></div>
+  <div><img src="def-img.png" alt="img"></div>  
+    </div>
+
+    
+</div>
+
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,42 @@
+
+<head>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <nav class="flexnav">
+    
+
+   <div style>
+        <h1 style="color: #D9DCDA; font-size: 40px;">ROTARY </h1><h1 style="color: #545454; font-size: 40px">E-CLUB OF ALL STAR CAMANAVA</h1>
+    </div>
+    
+        
+            <div class="dropdown">
+                <button class="dropbtn"><i class="bi bi-list" style="color:#545454; right: 0; font-size: 50px;"></i></button>
+                <div class="dropdown-content">
+                  <a href="#">Profile</a>
+                  <a href="#">Logout</a>
+                
+                </div>
+              </div>
+            
+        
+</nav>
+
+
+    <div class="sidebar">
+
+        <button class="button2">MEDIA</button> <br>
+        <BUtton class="button2">BULLETIN</BUtton> <br>
+        <BUtton class="button2">EVENTS</BUtton> <br>
+
+    </div>
+
+</head>
+
+
